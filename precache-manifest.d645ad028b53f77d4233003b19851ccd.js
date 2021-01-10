ba009ad41d771faee00b71b900bf48fc541c7ed1
QuyNguyen <quycao2010@gmail.com>
2021-01-11 01:06:01 +0700

    Updates

---
+++ precache-manifest.d645ad028b53f77d4233003b19851ccd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8859009e45e1d61adf8a087d23e69f0a",
+    "url": "/react-todo-list-demo/index.html"
+  },
+  {
+    "revision": "9772cf85a5e8bd5910e3",
+    "url": "/react-todo-list-demo/static/css/main.45996b72.chunk.css"
+  },
+  {
+    "revision": "46786b145991d5172ce5",
+    "url": "/react-todo-list-demo/static/js/2.1e082df5.chunk.js"
+  },
+  {
+    "revision": "9772cf85a5e8bd5910e3",
+    "url": "/react-todo-list-demo/static/js/main.ad5b555b.chunk.js"
+  },
+  {
+    "revision": "abd073bf50bac4227b28",
+    "url": "/react-todo-list-demo/static/js/runtime~main.c5bca88f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-todo-list-demo/static/media/logo.5d5d9eef.svg"
+  }
+]);
